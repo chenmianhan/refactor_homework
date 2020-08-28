bcb48893040e4e5534a0f1791b4a88d4f0cbc044
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-28 21:06:54 +0800

    [James Chen]test: add test Should return 3 when calculate voyageRisk given voyage length 8 and zone is west-china

--- test/rankTest.js
+++ test/rankTest.js
@@ -9,5 +9,13 @@ test('Should return 5 when calculate voyageRisk given voyage length 0 and zone i
   const result = voyageRisk(voyage)
   t.is(result, 5)
 });
+test('Should return 3 when calculate voyageRisk given voyage length 8 and zone is west-indies', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 8,
+  };
+  const result = voyageRisk(voyage)
+  t.is(result, 3)
+});
 
 
