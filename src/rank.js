2f8954b634d37b57f73e3ce132c31051d52e1c9d
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-28 22:16:53 +0800

    [James Chen]refactor: delete test code

--- src/rank.js
+++ src/rank.js
@@ -74,25 +74,3 @@ module.exports = {
   voyageRisk, hasChina, captainHistoryRisk, voyageProfitFactor, rating
 };
 
-const voyage = {
-  zone: 'west-indies',
-  length: 10,
-};
-const history = [
-  {
-    zone: 'east-indies',
-    profit: 5,
-  },{
-    zone: 'west-indies',
-    profit: 15,
-  },{
-    zone: 'china',
-    profit: -2,
-  },
-  {
-    zone: 'west-africa',
-    profit: 7,
-  },
-];
-const myRating = rating(voyage, history);
-console.log(`myRating: ${myRating}`);
