a970e671239eb7fbb601de5f9990e471bdbc1bfc
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-28 22:15:25 +0800

    [James Chen]refactor: simplify the double if logic

--- src/rank.js
+++ src/rank.js
@@ -1,11 +1,6 @@
 function voyageRisk (voyage) {
   let result = 1;
-  if (voyage.length > 4) {
-    result += 2;
-  }
-  if (voyage.length > 8) {
-    result += voyage.length - 8;
-  }
+  result += voyage.length > 8 ? voyage.length - 6 : (voyage.length > 4 ? 2 : 0)
   const zoneList = [
     'china',
     'east-indies',
