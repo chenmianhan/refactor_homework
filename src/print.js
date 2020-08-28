2df994c4a911aac5bea5e128611b02efdced3f04
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-28 22:55:36 +0800

    [James Chen]refactor: extract method calculateOutStanding

--- src/print.js
+++ src/print.js
@@ -1,16 +1,21 @@
-function printOwing(invoice) {
-  let result = `***********************\n**** Customer Owes ****\n***********************\n`
-  let outstanding = 0;
-  for (const o of invoice.borderSpacing) {
-    outstanding += o.amount;
-  }
+function calculateOutStanding(invoice) {
+    let outstanding = 0;
+    for (const o of invoice.borderSpacing) {
+        outstanding += o.amount;
+    }
+    return outstanding;
+}
 
-  const today = new Date();
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-  result += `name: ${invoice.customer}\n`;
-  result += `amount: ${outstanding}\n`;
-  result += `amount: ${invoice.dueDate.toLocaleDateString()}`;
-  return result;
+function printOwing(invoice) {
+    let result = `***********************\n**** Customer Owes ****\n***********************\n`
+    let outstanding = calculateOutStanding(invoice);
+
+    const today = new Date();
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    result += `name: ${invoice.customer}\n`;
+    result += `amount: ${outstanding}\n`;
+    result += `amount: ${invoice.dueDate.toLocaleDateString()}`;
+    return result;
 }
 
 module.exports = {printOwing};
