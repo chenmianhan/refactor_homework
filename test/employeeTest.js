2072e1de2ca4192a0e74a02ae53418bca62f654d
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-28 20:38:01 +0800

    [James Chen]test:add test Should return name and type when toString given a employee

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -7,3 +7,7 @@ test('Should not throw Error when new a Employee given right type manager', t =>
 test('Should  throw Error Employee cannot be of type student when new a Employee given wrong type student', t => {
     t.throws(() => new Employee('James', 'student'), "Employee cannot be of type student")
 })
+test('Should return name and type when toString given a employee', t => {
+    let employee = new Employee('James', 'manager');
+    t.is(employee.toString(), 'James (manager)')
+})
