64b89454cb413dc5a4ab99b2a5de3f2582f18bf2
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-28 19:44:37 +0800

    [James Chen]refactor: extract method getPlusDaysWhenIsNoRush

--- src/delivery.js
+++ src/delivery.js
@@ -16,30 +16,33 @@ function getPlusDaysWhenRush(anOrder) {
   return anOrder.placedOn.plusDays(1 + deliveryTime);
 }
 
+function getPlusDaysWhenIsNoRush(anOrder) {
+  let deliveryTime;
+  if ([
+    'MA',
+    'CT',
+    'NY',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  } else if ([
+    'ME',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 3;
+  } else {
+    deliveryTime = 4;
+  }
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
 function deliveryDate(anOrder, isRush) {
   if (isRush) {
     return getPlusDaysWhenRush(anOrder);
   } else {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    return getPlusDaysWhenIsNoRush(anOrder);
   }
 }
+
 module.exports = {
   deliveryDate
 };
