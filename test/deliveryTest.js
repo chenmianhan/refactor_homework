d04b0aff455f65a9a77c5c2ca378d1468faf33d9
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-28 18:53:04 +0800

    [James Chen]test: add delivery test case 1:is Rush,and delivery include MA.

---
+++ test/deliveryTest.js
@@ -0,0 +1,17 @@
+import {deliveryDate} from "../src/delivery";
+
+const  test = require('ava');
+test('delivery test case 1. is Rush,and delivery include MA. ', t => {
+    const anOrder = {
+        "deliveryState": 'MA',
+        "placedOn":{
+            "plusDays": function (plusDays) {
+                return plusDays;
+            }
+        }
+    }
+
+    const result = deliveryDate(anOrder,true);
+
+    t.is(result, 2);
+});
