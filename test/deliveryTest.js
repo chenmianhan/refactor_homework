b6a84ac641388c5f6168d2c8f97f045af348aa9d
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-28 18:55:15 +0800

    [James Chen]test: add delivery test case 1:is Rush,and delivery include NH.

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -29,3 +29,31 @@ test('delivery test case 1. is Rush,and delivery include CT. ', t => {
 
     t.is(result, 2);
 });
+test('delivery test case 1. is Rush,and delivery include NY. ', t => {
+    const anOrder = {
+        "deliveryState": 'NY',
+        "placedOn": {
+            "plusDays": function (plusDays) {
+                return plusDays;
+            }
+        }
+    }
+
+    const result = deliveryDate(anOrder, true);
+
+    t.is(result, 3);
+});
+test('delivery test case 1. is Rush,and delivery include NH. ', t => {
+    const anOrder = {
+        "deliveryState": 'NH',
+        "placedOn": {
+            "plusDays": function (plusDays) {
+                return plusDays;
+            }
+        }
+    }
+
+    const result = deliveryDate(anOrder, true);
+
+    t.is(result, 3);
+});
